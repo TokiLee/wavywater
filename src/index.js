e3c72f12646d89ac2d6e65ab14c69a3515463e7f
Toki <tokilee.92@gmail.com>
2018-09-08 22:00:27 -0400

    webpacked xd

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi'
+import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2'
+import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split'
+import './phaser-plugin-isometric';
+import './game.js';
